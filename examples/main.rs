1a4c5c87f311d9c6879403831bab837aa64d1ac0
Mark Anthony <markanthonymartinez9@gmail.com>
2019-11-29 13:31:57 -0600

    Rework example to use closure

--- examples/main.rs
+++ examples/main.rs
@@ -1,9 +1,23 @@
 extern crate olc_console_engine_oop;
 
 use olc_console_engine_oop::OlcConsoleGameEngine;
+use std::convert::TryInto;
+use winapi::um::winnt::SHORT;
+
+use rand::random;
 
 fn main() {
-    let mut demo = OlcConsoleGameEngine::new();
+    let test_clousre: Box<dyn FnMut(&mut OlcConsoleGameEngine)> = Box::new(|data| {
+        for x in 0..data.screen_width as usize {
+            for y in 0..data.screen_height as usize {
+                let ran_num = random::<u16>();
+                let conv = ran_num % 16;
+                data.draw(x, y, '@' as SHORT, conv.try_into().unwrap());
+            }
+        }
+    });
+
+    let mut demo = OlcConsoleGameEngine::new(test_clousre);
 
     demo.consturct_console(130, 60, 8, 16);
 
