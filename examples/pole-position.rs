762a6aed455aef96faf9918670d9283545c45851
Mark Anthony <markanthonymartinez9@gmail.com>
2019-12-10 13:02:25 -0600

    Update pole-position example

--- examples/pole-position.rs
+++ examples/pole-position.rs
@@ -4,18 +4,51 @@ use rustyConsoleGameEngine::OlcConsoleGameEngine;
 
 use std::thread;
 
+struct PolePole {
+    car_position_test: f64,
+    car_distance: f64,
+    track: Vec<(f64, f64)>,
+}
+
 fn main() {
     let mut init = true;
 
-    let closure: Box<dyn FnMut(&mut OlcConsoleGameEngine)> = Box::new(move |data| {
+    let mut game_data = PolePole {
+        car_position_test: 0.0,
+        car_distance: 0.0,
+        track: Vec::new(),
+    };
+
+    game_data.track.push((0.0, 10.0)); // Short section for start/finish line
+    game_data.track.push((0.0, 200.0));
+    game_data.track.push((1.0, 200.0));
+    game_data.track.push((0.0, 400.0));
+    game_data.track.push((-1.0, 100.0));
+    game_data.track.push((0.0, 200.0));
+    game_data.track.push((-1.0, 200.0));
+    game_data.track.push((1.0, 200.0));
+    game_data.track.push((0.0, 200.0));
+    game_data.track.push((0.2, 500.0));
+    game_data.track.push((0.0, 200.0));
+
+    let off_set = 0.0;
+    let track_section = 0;
+
+    let closure: Box<dyn FnMut(&mut OlcConsoleGameEngine<PolePole>)> = Box::new(move |data| {
+        if data.keys[0x26].held {
+            data.game_struct[0].car_distance += 100.0 * data.time_delta;
+        }
+
         if init {
             data.fill(0, 0, data.screen_width as usize, data.screen_height as usize, ' ' as i16, 0);
             init = false;
         } else {
             for y in 0..data.screen_height / 2 {
                 for x in 0..data.screen_width {
+                    let perspective = y as f64 / (data.screen_height as f64 / 2.0);
+
                     let middle_point = 0.5;
-                    let mut road_width = 0.6;
+                    let mut road_width = 0.1 + perspective * 0.8;
                     let clip_width = road_width * 0.15;
 
                     road_width *= 0.5;
@@ -28,12 +61,32 @@ fn main() {
 
                     let row = data.screen_height / 2 + y;
 
+                    let grass_calc = 20.0 * f64::powf(1.0 - perspective, 3.0) + data.game_struct[0].car_distance * 0.1;
+
+                    let clip_calc = 40.0 * f64::powf(1.0 - perspective, 2.0) + data.game_struct[0].car_distance;
+
+                    let grass_color;
+
+                    if grass_calc.sin() > 0.0 {
+                        grass_color = 0x000A;
+                    } else {
+                        grass_color = 0x0002;
+                    }
+
+                    let clip_color;
+
+                    if clip_calc.sin() > 0.0 {
+                        clip_color = 0x0000;
+                    } else {
+                        clip_color = 0x000F;
+                    }
+
                     if x < left_grass as i16 {
-                        data.draw(x as usize, row as usize, 0x2588, 0x000D);
+                        data.draw(x as usize, row as usize, 0x2588, grass_color);
                     }
 
                     if x >= left_grass as i16 && x < left_clip as i16 {
-                        data.draw(x as usize, row as usize, 0x2588, 0x000B);
+                        data.draw(x as usize, row as usize, 0x2588, clip_color);
                     }
 
                     if x >= left_clip as i16 && x < right_clip as i16 {
@@ -41,16 +94,16 @@ fn main() {
                     }
 
                     if x >= right_clip as i16 && x < right_grass as i16 {
-                        data.draw(x as usize, row as usize, 0x2588, 0x000B);
+                        data.draw(x as usize, row as usize, 0x2588, clip_color);
                     }
 
                     if x >= right_grass as i16 && x < data.screen_width {
-                        data.draw(x as usize, row as usize, 0x2588, 0x000D)
+                        data.draw(x as usize, row as usize, 0x2588, grass_color)
                     }
                 }
             }
 
-            let mut car_position = 0.0;
+            let mut car_position = data.game_struct[0].car_position_test;
 
             car_position = data.screen_width as f64 / 2.0 + ((data.screen_width as f64 * car_position) / 2.0) - 7.0;
 
@@ -64,7 +117,7 @@ fn main() {
         }
     });
 
-    let mut demo = OlcConsoleGameEngine::new(closure);
+    let mut demo = OlcConsoleGameEngine::new(closure, game_data);
 
     demo.consturct_console(150, 50, 6, 12);
 
