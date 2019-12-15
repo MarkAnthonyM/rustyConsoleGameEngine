d237457f153b22d566aeed1964e70dcfea98390b
Mark Anthony <markanthonymartinez9@gmail.com>
2019-12-15 02:28:48 -0600

    Add matrix example

---
+++ examples/matrix.rs
@@ -0,0 +1,19 @@
+extern crate rustyConsoleGameEngine;
+
+use rustyConsoleGameEngine::OlcConsoleGameEngine;
+
+struct MatrixStrut {
+
+}
+
+fn main() {
+    let matrix = MatrixStrut {
+
+    };
+
+    let closure = box::new(move |data| {
+
+    });
+
+    let demo = OlcConsoleGameEngine::new(closure, matrix);
+}
